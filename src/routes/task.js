470429b97884f263cd65d01df9f55638056cc3a7
Ryan Park <ryan.developer99@gmail.com>
2021-07-29 15:15:01 +0900

    add: task reading endpoints

--- src/routes/task.js
+++ src/routes/task.js
@@ -12,4 +12,27 @@ router.post("/tasks", async (req, res) => {
   }
 });
 
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).send(tasks);
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
+router.get("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(400).send();
+    }
+
+    res.status(200).send(task);
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
