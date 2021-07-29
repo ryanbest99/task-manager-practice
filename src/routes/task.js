74e7abd24c836bf2637b3224f48e77033650c1f3
Ryan Park <ryan.developer99@gmail.com>
2021-07-29 15:22:06 +0900

    add: task updating endpoints

--- src/routes/task.js
+++ src/routes/task.js
@@ -35,4 +35,36 @@ router.get("/tasks/:id", async (req, res) => {
   }
 });
 
+router.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  console.log(updates);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid Updates" });
+  }
+  try {
+    //     const task = await Task.findByIdAndUpdate(
+    //       req.params.id,
+    //       { $set: req.body },
+    //       { new: true }
+    //     );
+
+    const task = await Task.findById(req.params.id);
+    updates.forEach((update) => (task[update] = req.body[update]));
+    await task.save();
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send(task);
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
